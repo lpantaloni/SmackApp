5314bacc6b3380aa3019eca180413e13d4cd8846
Laurent Pantaloni <lpantaloni@mymacbook.home>
2018-09-19 09:34:33 +0200

    Swipe branch working

---
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -0,0 +1,36 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Laurent Pantaloni on 18/09/2018.
+//  Copyright © 2018 Laurent Pantaloni. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 80
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Smack/Smack/Controller/ChatVC.swift
@@ -0,0 +1,32 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Laurent Pantaloni on 18/09/2018.
+//  Copyright © 2018 Laurent Pantaloni. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
