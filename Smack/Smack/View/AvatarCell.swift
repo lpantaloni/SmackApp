8a79c394e589c1e42e2e1544f02bc62e7f49cad2
Laurent Pantaloni <lpantaloni@mymacbook.home>
2018-09-24 17:37:43 +0200

    Avatar Collection View created

---
+++ Smack/Smack/View/AvatarCell.swift
@@ -0,0 +1,25 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Laurent Pantaloni on 24/09/2018.
+//  Copyright © 2018 Laurent Pantaloni. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
