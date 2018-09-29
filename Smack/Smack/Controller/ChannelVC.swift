a8d9c0f504a9ed3c7ef67e1902f0ee60f8a3e0a7
Laurent Pantaloni <lpantaloni@mymacbook.home>
2018-09-29 16:36:34 +0200

    User Creation & Avatar saved

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -12,6 +12,8 @@ class ChannelVC: UIViewController {
     
     // Outlets
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var userImg: CircleImage!
+    
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
         
     }
@@ -20,6 +22,7 @@ class ChannelVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 80
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChanged(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
 
         // Do any additional setup after loading the view.
     }
@@ -29,6 +32,19 @@ class ChannelVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @objc func userDataDidChanged(_ notif: Notification) {
+        // uodate UI
+        // User
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
+    }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
---
+++ Smack/Smack/View/CircleImage.swift
@@ -0,0 +1,29 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Laurent Pantaloni on 28/09/2018.
+//  Copyright © 2018 Laurent Pantaloni. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+
+}
