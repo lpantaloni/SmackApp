af7191676ebcd18cb9b1133e7f1d4409b505e17b
Laurent Pantaloni <lpantaloni@mymacbook.home>
2018-10-08 15:00:08 +0200

    Channels diusplay handling

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class ChannelVC: UIViewController {
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     
     // Outlets
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var userImg: CircleImage!
-    
+    @IBOutlet weak var tableView: UITableView!
+
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
         
     }
@@ -21,6 +23,8 @@ class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 80
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChanged(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
 
@@ -66,6 +70,26 @@ class ChannelVC: UIViewController {
         }
 
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configureCell(channel: channel)
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+
 
     /*
     // MARK: - Navigation
