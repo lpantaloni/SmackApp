28b87e1682cd262496a7f040b35d88242dfbcfdc
Laurent Pantaloni <lpantaloni@MyMacbook.local>
2018-10-13 22:13:41 +0200

    Display existing messages

---
+++ Smack/Smack/View/MessageCell.swift
@@ -0,0 +1,40 @@
+//
+//  MessageCell.swift
+//  Smack
+//
+//  Created by Laurent Pantaloni on 12/10/2018.
+//  Copyright © 2018 Laurent Pantaloni. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    // Outlets
+    @IBOutlet weak var userImage: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: NSLayoutConstraint!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(message: Message) {
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.userName
+        userImage.image = UIImage(named: message.userAvatar)
+        userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+    
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
